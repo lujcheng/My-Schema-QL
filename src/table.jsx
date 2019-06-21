05d148a4dc45f5612df5f1bb0c2c150fa8c85a89
Brian Murray <brianmurraysound@gmail.com>
2019-06-21 11:43:39 -0400

    errors in table component fixed

--- src/table.jsx
+++ src/table.jsx
@@ -2,45 +2,45 @@ import React, { Component } from 'react'
 
 class Table extends Component {
 
-    renderTableHeader() {
-      let header = Object.keys(this.props.tables.cars.data[0])
-      console.log("header ", header)
-      return header.map((key, index) => {
-         return <th key={index}>{key.toUpperCase()}</th>
-      })
-    }
+  renderTableHeader() {
+    let header = Object.keys(this.props.tables.cars.data[0])
+    console.log("header ", header)
+    return header.map((key, index) => {
+      return <th key={index}>{key.toUpperCase()
+      }</th>
+    })
+  }
 
-    renderTableData() {
-      let data = this.props.tables.cars.data
-        return data.map((value) => {
-          let items = Object.values(value)
-            
-            return <tr className="data-row">{items.map((item) => {
-              return <td>{item}</td>
-          })
-        } </tr>
+  renderTableData() {
+    let data = this.props.tables.cars.data
+    return data.map((value, index) => {
+    let items = Object.values(value)
+      return <tr key={index} className="data-row">{items.map((item, index) => {
+        return <td key={index} >{item}</td>
         })
-      }
+      }</tr>
+    })
+  }
 
-    render() {
-      
-      return (
-        <div>
-          <table className="schema-table" border='1'>
-            <thead>
-              <tr>
-                <th colspan="5" className="table-title">{Object.keys(this.props.tables)[0]}</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="header-row">
-                {this.renderTableHeader()}
-              </tr>
-                {this.renderTableData()}
-            </tbody>
-          </table>
-        </div>
-      )
+  render() {
+    
+    return (
+      <div>
+        <table className="schema-table" border='1'>
+          <thead>
+            <tr>
+              <th colSpan="5" className="table-title">{Object.keys(this.props.tables)[0]}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="header-row">
+              {this.renderTableHeader()}
+            </tr>
+              {this.renderTableData()}
+          </tbody>
+        </table>
+      </div>
+    )
    }
 }
 
