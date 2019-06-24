d9f0ca129d87763ba91bde7a96b4af3fa1751dc7
Brian Murray <brianmurraysound@gmail.com>
2019-06-24 19:12:07 -0400

    validation.js added and react-validation and validator installed

--- src/query.jsx
+++ src/query.jsx
@@ -1,25 +1,29 @@
 import React, { Component } from 'react'
+import {required, email} from './validations.js'
+import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
+
+
 
 class Query extends Component {
-
-
-    render() {
+    
+		render() {
    
       return (
-				<nav id="sub-nav-bar">
-					<form action="" method="POST" className="sub-nav-elements">
-						<p className="query-item query-tags">SELECT</p>
-						<input name="select" type="text" placeholder="*" className="query-item input-query" onChange={(e) => this.props.onChange(e, "select")} />
-						<p className="query-item query-tags">FROM</p>
-						<input name="from" type="text" placeholder="Table name" className="query-item input-query" onChange={(e) => this.props.onChange(e, "from")} />
-						<select className="dropdown-box">
-								<option value="join">JOIN</option>
-								<option value="on">ON</option>
-								<option value="where">WHERE</option>
-							</select>
-						<button type="submit" className="button add-button">+</button>
-					</form>
-				</nav>
+					// <nav id="sub-nav-bar">
+						<Form action="" method="POST" className="sub-nav-elements">
+							<p className="query-item query-tags">SELECT</p>
+							<Input name="select" type="text" placeholder="" className="query-item input-query" onChange={(e) => this.props.onChange(e, "select")} validations={[required, email]} />
+							<p className="query-item query-tags">FROM</p>
+							<input name="from" type="text" placeholder="Table name" className="query-item input-query" onChange={(e) => this.props.onChange(e, "from")} />
+							<select className="dropdown-box">
+									<option value="join">JOIN</option>
+									<option value="on">ON</option>
+									<option value="where">WHERE</option>
+								</select>
+							<button type="submit" className="button add-button">+</button>
+						</Form>
+					// </nav>
       )
    }
 }
